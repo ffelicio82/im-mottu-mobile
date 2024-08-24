b4d328bc521d966f6a4071b3be148c8cf82be6f7
ffelicio82 <fernando_felicio82@yahoo.com.br>
2024-08-24 17:36:42 -0300

    :sparkles: feat: file to group exported files in presentation module

---
+++ lib/presentation/presentation.dart
@@ -0,0 +1,3 @@
+export 'package:im_mottu_mobile/presentation/home/home.dart';
+export 'package:im_mottu_mobile/presentation/router/router.dart';
+export 'package:im_mottu_mobile/presentation/splash/splash.dart';
