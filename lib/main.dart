20c477573d5a3e5a1c696a62cb27c7c0ce0af994
ffelicio82 <fernando_felicio82@yahoo.com.br>
2024-08-24 19:57:26 -0300

    :recycle: refactor: alter initial page reference

--- lib/main.dart
+++ lib/main.dart
@@ -21,7 +21,7 @@ class MyApp extends StatelessWidget {
         ),
         useMaterial3: true,
       ),
-      initialRoute: '/',
+      initialRoute: RouteEnum.home.path,
       getPages: Routes.routes,
     );
   }
